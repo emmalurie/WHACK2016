d44370db0d415ba6a09069340b0c37a547bcceae
EL <emmalurie@Computer-1.local>
2016-02-20 22:23:39 -0500

    something for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (8000));
+app.set('port', (process.env.PORT || 8000));
 
 app.use(express.static(__dirname + '/public'));
 
